fc84b00cdf0136c6995912e8f3fa1c066891c6dc
Mohamed Alhamry <alhamrimohammed4@email.com>
2025-04-27 21:04:40 +0300

    update

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.get('/', (req, res) => {
   res.send('The server is running');
 });
 
-app.listen(3000, (req, res) => {
+app.listen(3000, () => {
     console.log('Listening on port 3500');
     console.log(`Your secret is ${process.env.SECRET_PASSWORD}`)
 });
